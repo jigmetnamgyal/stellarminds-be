cda23dc4e1d1bb17e3ec1b719a214cd6e4890d40
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-08-17 23:34:23 +0600

    added migration support

--- initializer/database.go
+++ initializer/database.go
@@ -1,7 +1,6 @@
 package initializer
 
 import (
-	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -13,10 +12,11 @@ var DB *gorm.DB
 func ConnectToDb() {
 	var err error
 	dsn := os.Getenv("DATABASE_URL")
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	fmt.Println(DB)
 	if err != nil {
 		log.Fatal("Error connecting to db")
 	}
+
+	DB = db
 }

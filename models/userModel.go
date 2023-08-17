1b471e24c683bb496f990cf45aaa7d1f021e5dda
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-08-17 23:04:15 +0600

    feat(): added flag for email validation

--- models/userModel.go
+++ models/userModel.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email           string `gorm:"unique; not null"`
+	Email           string `gorm:"unique; not null" validate:"email"`
 	Password        string
 	ConfirmPassword string
 	AgreeToTerms    bool

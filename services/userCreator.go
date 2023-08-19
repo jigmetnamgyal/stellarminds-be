5d37797e0717b66d1570bc4420cfce005aff5d57
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-08-19 23:28:29 +0600

    feat(): added profile creation in create-user api

--- services/userCreator.go
+++ services/userCreator.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"github.com/Stellar-Lab/stellarminds-be/initializer"
 	"github.com/Stellar-Lab/stellarminds-be/models"
 	"github.com/Stellar-Lab/stellarminds-be/services/helpers"
 	"github.com/gin-gonic/gin"
+	validator2 "github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
@@ -29,50 +32,56 @@ func UserCreator(c *gin.Context) {
 		return
 	}
 
-	//validate := validator2.New()
-	//
-	//if validationError := validate.Struct(body); validationError != nil {
-	//	c.JSON(http.StatusBadRequest, gin.H{
-	//		"error": "Email is not valid",
-	//	})
-	//
-	//	return
-	//}
-	//
-	//if body.Password != body.ConfirmPassword {
-	//	c.JSON(http.StatusBadRequest, gin.H{
-	//		"error": "Confirm password doesn't match your password",
-	//	})
-	//
-	//	return
-	//}
-	//
-	//hash, hashError := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
-	//
-	//if hashError != nil {
-	//	c.JSON(http.StatusBadRequest, gin.H{
-	//		"error": "Failed to hash password",
-	//	})
-	//
-	//	return
-	//}
-	//
-	//user := models.User{
-	//	Email:           body.Email,
-	//	Password:        string(hash),
-	//	ConfirmPassword: string(hash),
-	//	AgreeToTerms:    body.AgreeToTerms,
-	//}
-	//
-	//result := initializer.DB.Create(&user)
-	//
-	//if result.Error != nil {
-	//	c.JSON(http.StatusBadRequest, gin.H{
-	//		"error": "Failed to create User",
-	//	})
-	//
-	//	return
-	//}
-	//
-	//c.JSON(http.StatusOK, user)
+	validate := validator2.New()
+
+	if validationError := validate.Struct(body); validationError != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Email is not valid",
+		})
+
+		return
+	}
+
+	if body.Password != body.ConfirmPassword {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Confirm password doesn't match your password",
+		})
+
+		return
+	}
+
+	hash, hashError := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+
+	if hashError != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to hash password",
+		})
+
+		return
+	}
+
+	user := models.User{
+		Email:           body.Email,
+		Password:        string(hash),
+		ConfirmPassword: string(hash),
+		AgreeToTerms:    body.AgreeToTerms,
+		Profile: models.Profile{
+			Name:        body.Profile.Name,
+			DateOfBirth: body.Profile.DateOfBirth,
+			Gender:      body.Profile.Gender,
+			AvatarUrl:   body.Profile.AvatarUrl,
+		},
+	}
+
+	result := initializer.DB.Create(&user)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create User",
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
